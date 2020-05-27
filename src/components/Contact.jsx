c1574739b1b62246f7ce926b0082354f67ec4d57
Letícia Vargas <lekkinhah@gmail.com>
2020-05-27 00:09:13 -0300

    Update modules

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import './Contact.scss';
 
 class Contact extends React.Component {
+  
   render() {
-    return (<div />);
+    const {data} = this.props;
+    return (
+          <article className="contact" data-testid="contact">
+            <span className="contact__avatar">
+              <img src={data.avatar} alt={data.name} />
+            </span>
+            <span className="contact__data">{data.name}</span>
+            <span className="contact__data">{data.phone}</span>
+            <span className="contact__data">{data.country}</span>
+            <span className="contact__data">{data.admissionDate}</span>
+            <span className="contact__data">{data.company}</span>
+            <span className="contact__data">{data.departament}</span>
+          </article>    
+      );
   }
 }
 
